d3834544e558f01646538f7198070b3ffb67166c
Tharusha-Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2023-12-13 15:03:47 +0530

    Project Architecture initialisation

---
+++ Client-Server/Flower-Exchange-Tharusha/Trader.cpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "Trader.h"
---
+++ Client-Server/Flower-Exchange-Tharusha/exchanger_main.cpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "Order.h"
+#include "Trader.h"
+#include "Exchange.h"
+#include "OrderBook.h"
+
+int main(int argc, char* argv[]) {
+    return 0;
+}

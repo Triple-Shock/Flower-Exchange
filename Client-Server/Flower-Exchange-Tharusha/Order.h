463e96875e515057b5b7206f36d1f6bde628cfb1
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2023-12-13 15:11:19 +0530

    Adding Order and Orderbook classes

--- Client-Server/Flower-Exchange-Tharusha/Order.h
+++ Client-Server/Flower-Exchange-Tharusha/Order.h
@@ -1,1 +1,43 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <ctime>
+
+constexpr auto OUTFILE = "execution_rep.csv";
+
+class Order
+{
+private:
+    void helper(int, double);
+
+public:
+    std::string order_id;
+    std::string client_order_id;
+    std::string instrument;
+    int side;
+    int exec_status;
+    int quantity;
+    double price;
+    std::string reason;
+    std::string transactionTime;
+
+    Order(int, std::string, std::string, int, int, int, double);
+
+    void write_csv();
+    void write_csv(int);
+    void write_csv(double);
+    void write_csv(int, double);
+
+    bool is_valid();
+    std::string transaction_time();
+};
+
+void printOrders(const std::vector<Order>& orders);
--- Client-Server/Flower-Exchange-Tharusha/OrderBook.h
+++ Client-Server/Flower-Exchange-Tharusha/OrderBook.h
@@ -1,2 +1,18 @@
 #pragma once
+#include "Order.h"
 
+class OrderBook
+{
+    public:
+        std::string instrument;
+        std::vector<Order> buy;
+        std::vector<Order> sell;
+        void trade(Order & order);
+
+        OrderBook(std::string);
+        void print();
+
+    private:
+        static bool buy_compare(Order a, Order b);
+        static bool sell_compare(Order a, Order b);
+};
